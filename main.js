c5525284b8d57b0dfd00a3488d871060ff998ec5
Tony <mohammedadell496@gmail.com>
2024-07-10 01:26:35 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+// let play = document.getElementById("play")
+// let playBtn = document.querySelector(".play-btn a")
+//  function clicked(){
+//     play.addClass('active-popup')
+// }
+
+
+// $(document).ready(function(){
+//     $(document).on('click', '.play-btn a', function(){
+//         $('#play').addClass('on');
+//     });
+// });
